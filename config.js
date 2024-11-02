59496745e27f57d81dd2181aeace168afa6103ea
vodycool <voldysad@gmail.com>
2024-11-02 06:11:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "zB8RTBZJ#21ooF2xrEzkYvEMthSi9pLtctSHVFOhf3RZD5-TRKcQ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
